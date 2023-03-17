6749ea2759e09c5a32b81ef46b06935ddfbd6b51
AmirHossein-Khanestani <a.khanestani@gmail.com>
2023-03-16 23:22:14 -0700

    finish header

--- lib/main.dart
+++ lib/main.dart
@@ -5,25 +5,37 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  static const defaultFontFamily = 'Avenir';
   const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    final primaryTextColor = Color(0xff0d253c);
+    final secondaryTextColor = Color(0xff2d4379);
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted.
+          primarySwatch: Colors.blue,
+          textTheme: TextTheme(
+            titleMedium: TextStyle(
+                fontFamily: defaultFontFamily,
+                color: secondaryTextColor,
+                fontSize: 15),
+            titleLarge: TextStyle(
+                fontFamily: defaultFontFamily,
+                fontWeight: FontWeight.bold,
+                color: primaryTextColor),
+          )),
       home: const HomeScreen(),
     );
   }
@@ -35,8 +47,38 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.fromLTRB(32, 16, 32, 0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Hi,Jonathan!',
+                      style: Theme.of(context).textTheme.titleMedium,
+                    ),
+                    Image.asset(
+                      'assets/img/icons/notification.png',
+                      width: 24,
+                      height: 24,
+                    )
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(32, 0, 0, 24),
+                child: Text(
+                  'Explore today',
+                  style: Theme.of(context).textTheme.titleLarge,
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
